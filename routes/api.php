1a39c667d591e0d02a932ad1f178a2bc780a8fc8
Mohamedkhalil1 <mohamedkhalil125445@gmail.com>
2023-08-06 11:45:19 +0300

    intial the routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/visits', 'VisitController@index');
+Route::get('/visits/{visit}', 'VisitController@show');
+Route::post('/visits', 'VisitController@store');
+Route::put('/visits/{visit}', 'VisitController@update');
+Route::delete('/visits/{visit}', 'VisitController@destroy');
+
+Route::get('/members', 'MemberController@index');
+Route::post('/members', 'MemberController@store');
+Route::put('/members/{member}', 'MemberController@update');
+Route::delete('/members/{member}', 'MemberController@destroy');
